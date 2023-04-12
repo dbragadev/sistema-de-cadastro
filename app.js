eb3d416a42a9895e9ab513f11f214b49fc9f5f3d
daniel <dbragadev@gmail.com>
2023-04-12 17:34:17 -0300

    adicionando um comentário no app.js

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log ("Rodando o sistema de gerenciamento de jogos.")
 console.log ("aplicação está se conectando ao banco de dados.")
+// mais um console log para teste
+console.log ("Este é um teste!")
